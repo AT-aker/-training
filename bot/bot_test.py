153199a5a8df0e8610ac5bf9dad686b5cd7075a4
Anatolii Kukoviakin <a.kukoviakin@gmail.com>
2020-10-19 18:34:26 +0300

    sume update

--- bot/bot_test.py
+++ bot/bot_test.py
@@ -4,10 +4,12 @@ from aiogram.utils import executor
 bot = Bot(token="1330106143:AAHpIavxj4NjPUq3T_GUxi3Xnw8W_myvGRM")
 dp = Dispatcher(bot)
 
+
 @dp.message_handler()
-async def get_message(message:types.Message)
+async def get_message(message: types.Message):
     chat_id = message.chat.id
     text = 'Hi my little friend'
-    await bot.send_message(chat_id=chat_id, text=text)
-executor.start_polling(dp)
+    sent_message = await bot.send_message(chat_id=chat_id, text=text)
+    print(sent_message.to_python())
 
+executor.start_polling(dp)
